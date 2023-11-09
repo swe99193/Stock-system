331a62e2d5246668497782b826fa3476ca42c1ed
Kevin Hsiao <khsiao.33@gmail.com>
2023-11-10 03:21:19 +0800

    update ratio_generator_lib.py

--- Parser-system/ratio_generator_lib.py
+++ Parser-system/ratio_generator_lib.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import traceback
 import pandas as pd
 import os
 import subprocess
@@ -6,42 +7,42 @@ import sqlite3
 import yfinance as yf
 
 # important index for calculating ratios
-CodeSet = {
-        '1170',
-        '11XX',
-        '130X',
-        '15XX',
-        '1600',
-        '1780',
-        '1900',
-        '1XXX',
-        '21XX',
-        '25XX',
-        '2600',
-        '3XXX',
-        '3100',
-        '3120',
-        '4000',
-        '5000',
-        '5900',
-        '6900',
-        '7900',
-        '8200',
-        # 'A20100',
-        # 'A20200',
-        'AAAA',
-        'B02700'}
-# (for reference)
-FinCompCodeSet = {
-        '1170',
-        '1780', 
-        '1XXX',
-        '3100',
-        '3XXX',
-        '7900',
-        '8200',
-        'AAAA',
-        'B02700'}
+CodeMap = {
+        '1170': 'Receivables', # 應收帳款
+        '11XX': 'Total Current Asset', # 流動資產
+        '130X': 'Inventory', # 存貨
+        '15XX': 'Total Non-Current Assets', # 非流動資產
+        '1600': '不動產', # 不動產、廠房及設備
+        '1780': 'Intangible Assets', # 無形資產
+        '1900': 'Other Non-Current Assets', # 其他非流動資產
+        '1XXX': 'Total Assets', # 資產總計
+        '21XX': 'Total Current Liabilities', # 流動負債
+        '25XX': 'Total Non-Current Liabilities', # 非流動負債
+        '2600': 'Other Non-Current Liabilities', # 其他非流動負債
+        '3XXX': 'Total Equity', # 權益總額
+        '3100': 'Shareholder Equity', # 股本合計
+        '3120': 'Preferred Stock', # 特別股股本
+        '4000': 'Revenue', # 營業收入
+        '5000': 'Cost Of Goods Sold', # 營業成本
+        '5900': 'Gross Profit', # 營業毛利
+        '6900': 'Operating Income', # 營業利益
+        '7900': 'Pre-Tax Income', # 繼續營業單位稅前淨利
+        '8200': 'Net Income', # 本期淨利
+        'AAAA': 'Operating Cash Flow', # 營業活動之淨現金流入
+        'B02700': 'Capital Expenditures' # 取得不動產、廠房及設備
+        }
+FinanceCompanyCodeMap = {
+        '1170': 'Receivables',
+        '1780': 'Intangible Assets', 
+        '1XXX': 'Total Assets',
+        '3100': 'Shareholder Equity',
+        '3XXX': 'Total Equity',
+        '3120': 'Preferred Stock',
+        '7900': 'Pre-Tax Income',
+        '8200': 'Net Income',
+        'AAAA': 'Operating Cash Flow',
+        'B02700': 'Capital Expenditures'
+        }
 
 table_name = lambda param: '_'.join(['t', param[0], param[1], param[2]])
 
@@ -59,11 +60,11 @@ Q4_END = ('-05-19', '-05-24')
 
 BASE_ID = '0050.TW' # 大盤 id
 
-INPUT_DF = pd.read_csv(os.path.join('..', 'csv', 'target_company.csv')) # target company ids
+INPUT_DF = pd.read_csv(os.path.join('..', 'csv', 'target_company.csv'), dtype="str") # target company ids
 
 FINAL_TABLE = 't_final_data'
 
-f = open(os.path.join('log', "warning.txt"), "a")
+logger = open(os.path.join('log', "ratio_generator.txt"), "a")
 
 
 def _stock_price_helper(id, Y, date_suffix):
@@ -74,7 +75,7 @@ def _stock_price_helper(id, Y, date_suffix):
     return df['Close'][0] # take the first available row
 
 
-def stock_return_parser(param, connection):
+def stock_return_parser(param, cursor):
     '''
         Given company id, year and quarter, calculate the stock return and store it into sqlite table
 
@@ -82,39 +83,36 @@ def stock_return_parser(param, connection):
             id(str): id of target company
             Year(str)
             Quarter(str)
-        connection: sqlite
+        cursor: sqlite cursor
 
         Note: If you call this function with the same parameters again, the original data will be replaced by new data.
 
         Note: company id should NOT be provided with ".TW" suffix
     '''
-    cursor = connection.cursor()
     id, Year, Quarter = param
-    id += '.TW'
-
-    print(f'\nCalculating Stock Return of {id}, {Year}, {Quarter}')
+    query_id = id + '.TW'
 
     growth_rate = lambda start, end: (end - start)/start
-    incr = lambda Y: str(int(Year) + 1) # year increment in string
+    year_increment = lambda year: str(int(year) + 1) # year increment in string
 
     if Quarter == '1':
-        start = _stock_price_helper(id, Year, Q1_START)    # 個股收盤價
-        end = _stock_price_helper(id, Year, Q1_END)
+        start = _stock_price_helper(query_id, Year, Q1_START)    # 個股收盤價
+        end = _stock_price_helper(query_id, Year, Q1_END)
         # b_start = _stock_price_helper(BASE_ID, Year, Q1_START) # 大盤收盤價
         # b_end = _stock_price_helper(BASE_ID, Year, Q1_END)
     elif Quarter == '2':
-        start = _stock_price_helper(id, Year, Q2_START)
-        end = _stock_price_helper(id, Year, Q2_END)
+        start = _stock_price_helper(query_id, Year, Q2_START)
+        end = _stock_price_helper(query_id, Year, Q2_END)
         # b_start = _stock_price_helper(BASE_ID, Year, Q2_START)
         # b_end = _stock_price_helper(BASE_ID, Year, Q2_END)
     elif Quarter == '3':
-        start = _stock_price_helper(id, Year, Q3_START)
-        end = _stock_price_helper(id, incr(Year), Q3_END)
+        start = _stock_price_helper(query_id, Year, Q3_START)
+        end = _stock_price_helper(query_id, year_increment(Year), Q3_END)
         # b_start = _stock_price_helper(BASE_ID, Year, Q3_START)
         # b_end = _stock_price_helper(BASE_ID, incr(Year), Q3_END)
     elif Quarter == '4':
-        start = _stock_price_helper(id, incr(Year), Q4_START)
-        end = _stock_price_helper(id, incr(Year), Q4_END)
+        start = _stock_price_helper(query_id, year_increment(Year), Q4_START)
+        end = _stock_price_helper(query_id, year_increment(Year), Q4_END)
         # b_start = _stock_price_helper(BASE_ID, incr(Year), Q4_START)
         # b_end = _stock_price_helper(BASE_ID, incr(Year), Q4_END)
     else:
@@ -135,88 +133,78 @@ def stock_return_parser(param, connection):
             '
     print('[QUERY] ' + query) 
     cursor.execute(query)
-    connection.commit()
 
 
-def generate_ratios(param, connection):
+def generate_ratios(param, cursor):
     '''
+        Generate financial indices from financial statements (from parser). 
+        
+        Different calculations for general companies & financial companies
+
+        param (str, str, str): (stock id, year, season) 
+        cursor: sqlite cursor
+        
         Note: If you call this function with the same parameters again, the original data will be replaced by new data.
 
         Note: company id should NOT be provided with ".TW" suffix
     '''
-    cursor = connection.cursor()
     table = table_name(param)
     id, Year, Quarter = param
-    id += '.TW'
     sector = INPUT_DF[INPUT_DF['CO_ID'] == id]['Sector'].values[0]
 
-    print(f'\n[TASK] Generating Ratios of {id}, {Year}, {Quarter}')
+    val = {}
 
-    val = {'3120' : 0, '1780' : 0}
-
-    # Also CHECK if every required column exists before calculating ratios
-    _codeSet = CodeSet if sector != 'Financial Services' else FinCompCodeSet
+    codeMap = CodeMap if sector != 'Financial Services' else FinanceCompanyCodeMap
     missing_columns = set()
 
-    for code in _codeSet:
+    for code, name in codeMap.items():
         query = f'SELECT Money FROM {table} WHERE Code = "{code}"'
         result = cursor.execute(query).fetchone()
         
-        # check missing columns
+        # check missing columns, and set zero for missing value
         if result is None:   
             # 忽略檢查: 特別股股本 '3120', 無形資產 '1780'
-            if code != '3120' and code != '1780':
-                missing_columns.add(code)
+            missing_columns.add(code)
+            val[name] = 0
         else:
-            val[code] = result[0]
+            val[name] = result[0]
 
     if missing_columns:
-        warning = f'These columns are missing: {missing_columns}'
-
-        f.write(f'[WARNING] {table}: ' + warning + '\n')
-        print(f'[WARNING] {warning}')
-        
-        while 1:
-            t = input('Continue? [y/n] ')
-            if t == 'y':
-                # set zero for missing value
-                for x in missing_columns:
-                    val[x] = 0
-                break
-            elif t == 'n':
-                raise Exception(warning)
-            else:
-                continue
-        
+        warning = f'[WARNING] 🔴 In {table} ,these columns are missing: {missing_columns}\n'
+        logger.write(warning)
+        print(warning)
+    
+    # if分母0, set zero
+    divide = lambda dividend, divisor: dividend / divisor if divisor != 0 else 0
 
     if sector != 'Financial Services':
-        val['Long Term Debt'] = val['25XX'] - val['2600']
-        val['Long Term Investment'] = val['15XX'] - val['1600'] - val['1780'] - val['1900']
-        val['Shares Outstanding'] = val['3100'] / 10    # 股本/10
+        val['Long Term Debt'] = val['Total Non-Current Liabilities'] - val['Other Non-Current Liabilities']
+        val['Long Term Investment'] = val['Total Non-Current Assets'] - val['不動產'] - val['Intangible Assets'] - val['Other Non-Current Assets']
+        val['Shares Outstanding'] = val['Shareholder Equity'] / 10    # 股本/10
         
         ratios = dict()
 
-        ratios["Current Ratio"] = val['11XX'] / val['21XX']
-        ratios["Long Term Debt to Capital Ratio"] = val['Long Term Debt']/(val['Long Term Debt'] + val['3XXX'])
-        ratios["Debt to Equity Ratio"] = val['Long Term Debt'] / val['3XXX']
-        ratios["Gross Margin"] = val['5900'] / val['4000']
-        ratios["Operating Margin"] = val['6900'] / val['4000']
-        ratios["Pre-Tax Profit Margin"] = val['7900'] / val['4000']
-        ratios["Net Profit Margin"] = val['8200'] / val['4000']
-        ratios["Asset Turnover"] = val['4000'] / val['1XXX']
-        ratios["Inventory Turnover Ratio"] = val['5000'] / val['130X']
-        ratios["Receivable Turnover"] = val['4000'] / val['1170']
-        ratios["Days Sales In Receivables"] = val['1170'] / val['4000'] * 365
-        ratios["ROE"] = val['8200'] / val['3XXX']
-        ratios["ROTE"] = val['8200'] / (val['3XXX'] - val['1780'] - val['3120'])
-        ratios["ROA"] = val['8200'] / val['1XXX']
-        ratios["ROI"] = val['8200'] / (val['Long Term Debt'] + val['Long Term Investment'])
-        ratios["Book Value Per Share"] = (val['3XXX'] - val['3120'])/ val['Shares Outstanding']
-        ratios["Operating Cash Flow Per Share"] = val['AAAA'] / val['Shares Outstanding']
-        ratios["Free Case Flow Per Share"] = (val['AAAA'] - val['B02700']) / val['Shares Outstanding']
+        ratios["Current Ratio"] = divide(val['Total Current Asset'], val['Total Current Liabilities'])
+        ratios["Long Term Debt to Capital Ratio"] = divide(val['Long Term Debt'], val['Long Term Debt'] + val['Total Equity'])
+        ratios["Debt to Equity Ratio"] = divide(val['Long Term Debt'], val['Total Equity'])
+        ratios["Gross Margin"] = divide(val['Gross Profit'], val['Revenue'])
+        ratios["Operating Margin"] = divide(val['Operating Income'], val['Revenue'])
+        ratios["Pre-Tax Profit Margin"] = divide(val['Pre-Tax Income'], val['Revenue'])
+        ratios["Net Profit Margin"] = divide(val['Net Income'], val['Revenue'])
+        ratios["Asset Turnover"] = divide(val['Revenue'], val['Total Assets'])
+        ratios["Inventory Turnover Ratio"] = divide(val['Cost Of Goods Sold'], val['Inventory'])
+        ratios["Receivable Turnover"] = divide(val['Revenue'], val['Receivables'])
+        ratios["Days Sales In Receivables"] = divide(val['Receivables'], val['Revenue']) * 365
+        ratios["ROE"] = divide(val['Net Income'], val['Total Equity'])
+        ratios["ROTE"] = divide(val['Net Income'], val['Total Equity'] - val['Intangible Assets'] - val['Preferred Stock'])
+        ratios["ROA"] = divide(val['Net Income'], val['Total Assets'])
+        ratios["ROI"] = divide(val['Net Income'], val['Long Term Debt'] + val['Long Term Investment'])
+        ratios["Book Value Per Share"] = divide(val['Total Equity'] - val['Preferred Stock'], val['Shares Outstanding'])
+        ratios["Operating Cash Flow Per Share"] = divide(val['Operating Cash Flow'], val['Shares Outstanding'])
+        ratios["Free Case Flow Per Share"] = divide(val['Operating Cash Flow'] - val['Capital Expenditures'], val['Shares Outstanding'])
 
     else:
-        val['Shares Outstanding'] = val['3100'] / 10    # 股本/10
+        val['Shares Outstanding'] = val['Shareholder Equity'] / 10    # 股本/10
         
         ratios = dict()
 
@@ -231,20 +219,16 @@ def generate_ratios(param, connection):
         ratios["Inventory Turnover Ratio"] = None
         ratios["Receivable Turnover"] = None
         ratios["Days Sales In Receivables"] = None
-        ratios["ROE"] = val['8200'] / val['3XXX']
-        ratios["ROTE"] = val['8200'] / (val['3XXX'] - val['1780'] - val['3120'])
-        ratios["ROA"] = val['8200'] / val['1XXX']
+        ratios["ROE"] = divide(val['Net Income'], val['Total Equity'])
+        ratios["ROTE"] = divide(val['Net Income'], val['Total Equity'] - val['Intangible Assets'] - val['Preferred Stock'])
+        ratios["ROA"] = divide(val['Net Income'], val['Total Assets'])
         ratios["ROI"] = None
-        ratios["Book Value Per Share"] = (val['3XXX'] - val['3120'])/ val['Shares Outstanding']
-        ratios["Operating Cash Flow Per Share"] = val['AAAA'] / val['Shares Outstanding']
-        ratios["Free Case Flow Per Share"] = (val['AAAA'] - val['B02700']) / val['Shares Outstanding']
+        ratios["Book Value Per Share"] = divide(val['Total Equity'] - val['Preferred Stock'], val['Shares Outstanding'])
+        ratios["Operating Cash Flow Per Share"] = divide(val['Operating Cash Flow'], val['Shares Outstanding'])
+        ratios["Free Case Flow Per Share"] = divide(val['Operating Cash Flow'] - val['Capital Expenditures'], val['Shares Outstanding'])
 
-    # FIXME
-    # for x in val:
-    #     print(f'{x}: {val[x]}')
-    # for x in ratios:
-    #     print(f'{x}: {ratios[x]}')
     # load ratios into sqlite table
+    # construct update query
     query = f'UPDATE {FINAL_TABLE} SET '
     
     for i, (key, val) in enumerate(ratios.items()):
@@ -255,19 +239,23 @@ def generate_ratios(param, connection):
         if i<len(ratios)-1: query += ', '
 
     query += f' WHERE CO_ID = "{id}" AND Year = "{Year}" AND Quarter = "{Quarter}"'
-    print('[QUERY] ' + query) 
+    print('[INFO] query: ' + query) 
     cursor.execute(query)
-    connection.commit()
-
 
 
 def generate_final_data(param, connection):
+    '''
+        Generate financial indices + stock return. 
+
+        param (str, str, str): (stock id, year, season) 
+        cursor: sqlite cursor
+
+        Note: company id should NOT be provided with ".TW" suffix
+    '''
     cursor = connection.cursor()
     id, Year, Quarter = param
-    id += '.TW'
 
-    print('----------------------------------------------------------')
-    print(f'Generating final data: {id}, {Year}, {Quarter}')
+    print(f'[INFO] 🟢 Start ratio generator')
 
     # initialize the row if not exists in final table (sqlite)
     if cursor.execute(f'SELECT * FROM {FINAL_TABLE} WHERE CO_ID = "{id}" AND Year = "{Year}" AND Quarter = "{Quarter}"').fetchone() is None:
@@ -275,13 +263,23 @@ def generate_final_data(param, connection):
         data = id, Year, Quarter, sector
 
         query = f'INSERT INTO {FINAL_TABLE} (CO_ID, Year, Quarter, Sector) VALUES (?,?,?,?);'
-        print('[QUERY] ' + query) 
+        print('[INFO] query: ' + query) 
         cursor.execute(query, data)
 
-        connection.commit()
+    try:
+        stock_return_parser(param, cursor)
+        generate_ratios(param, cursor)
+        
+    except Exception as e:
+        error_message = f'[ERROR] ❌ generate ratio: {table_name((id, Year, Quarter))}\n'
+        logger.write(error_message)
+        print(error_message)
+        traceback.print_exc()
+        # connection.rollback() # not needed
+        return
 
-    stock_return_parser(param, connection)
-    generate_ratios(param, connection)
 
-    print(f'\n[SUCCESS] Complete final data: {id}, {Year}, {Quarter}')
+    connection.commit()
+    print(f'\n[INFO] ✅ Complete final data: {id}, {Year}, {Quarter}')
     print('----------------------------------------------------------\n')
+    return
